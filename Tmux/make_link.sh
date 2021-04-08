81c7047f4a4a8e98a337795b67155d0b3d78f101
kskdev <kosumikedev@gmail.com>
2021-04-08 23:36:52 +0900

    remove gpu usage info bar and mod several funcs

--- Tmux/make_link.sh
+++ Tmux/make_link.sh
@@ -13,19 +13,21 @@
 
 # Delete already link.
 _TARGET1='.tmux.conf'
-_TARGET2='gpuinfo_tmux.sh'
 unlink ~/${_TARGET1}
-unlink ~/${_TARGET2}
-
 # リンクが存在しなければ，リンクを作成
 _SOURCE1=$(pwd)'/'${_TARGET1}
 if [ ! -e '~/'${_TARGET1} ]; then
   echo 'Create link : ' ${_SOURCE1}
-  ln -s ${_SOURCE1} ~/
+  ln -s ${_SOURCE1} ~/.tmux.conf
 fi
-_SOURCE2=$(pwd)'/'${_TARGET2}
-if [ ! -e '~/'${_TARGET2} ]; then
-  echo 'Create link : ' ${_SOURCE2}
-  ln -s ${_SOURCE2} ~/
-fi
+
+# # GPU 表示するわけじゃないので不要
+# _TARGET2='gpuinfo_tmux.sh'
+# unlink ~/${_TARGET2}
+# # リンクが存在しなければ，リンクを作成
+# _SOURCE2=$(pwd)'/'${_TARGET2}
+# if [ ! -e '~/'${_TARGET2} ]; then
+#   echo 'Create link : ' ${_SOURCE2}
+#   ln -s ${_SOURCE2} ~/
+# fi
 
