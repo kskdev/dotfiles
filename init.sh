9efbc034d669daa07558c0c09a3b2f6bb3ee9d06
kskdev <kosumikedev@gmail.com>
2021-04-10 16:57:55 +0900

    add init.sh for creating env.

---
+++ init.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# ::::::::::::::::::::::::::::::::::::::::
+# 環境構築 on Ubuntu
+# ::::::::::::::::::::::::::::::::::::::::
+
+
+# --- install apps
+echo -e "\e[31m ::: 1. Install apps via apt \e[m"
+cd ./Scripts/AppInit/
+sh init_ubuntu.sh
+cd ../../
+
+# --- setup tmux configs
+echo -e "\e[31m ::: 2. Setup tmux configs \e[m"
+cd ./Tmux/
+zsh make_link.sh
+cd ../
+
+# --- setup zsh configs
+echo -e "\e[31m ::: 3. Setup zsh configs \e[m"
+cd ./Shell/
+zsh zshrc_init.sh
+cd ../
+
+# --- setup vim configs
+echo -e "\e[31m ::: 4. Setup vim configs \e[m"
+cd ./Editor/vim/
+zsh install_vim_zsh_alone.sh
+cd ../../
+
+# --- setup others settings.
+echo -e "\e[31m ::: Ex. Setup others configs \e[m"
+echo "input password for changing login shell."
+chsh -s $(which zsh)
+echo "reload .zshrc"
+source ~/.zshrc
+
+echo " "
+echo " "
+echo -e "\e[31m Done setup. hello myworld. \e[m"
+echo ""
+echo " "
+echo " "
+
