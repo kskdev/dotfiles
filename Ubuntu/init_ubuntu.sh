eabb8073f3a377cf37428262b09d6abddd51e011
kskdev <kosumikedev@gmail.com>
2026-01-01 17:29:27 +0900

    update: ubuntu.

--- Ubuntu/init_ubuntu.sh
+++ Ubuntu/init_ubuntu.sh
@@ -3,10 +3,10 @@
 # インストール中に質問されないように設定
 export DEBIAN_FRONTEND=noninteractive
 
-# sudo コマンドが利用可能かチェックし、root の場合は sudo を空にする
-# 目的は，sudo コマンド不要の環境(既にroot環境)でも，sudo コマンドを実行できるようにする
+# sudo コマンドが利用可能かチェックし、root の場合は sudo を関数として空定義する
 if [ "$(id -u)" -eq 0 ]; then
-    alias sudo=''
+    # rootの場合はsudoを何もしない関数として定義
+    sudo() { "$@"; }
 elif ! command -v sudo > /dev/null; then
     echo "This script requires sudo. Please install sudo or run as root."
     exit 1
@@ -18,10 +18,10 @@ fi
 # パッケージリストの更新
 sudo apt update
 
-# 各セットアップスクリプトの実行
-sh ./essential.sh
-sh ./development.sh
-sh ./utility_apps.sh
+# 各セットアップスクリプトの実行（ドットコマンドで現在の環境を引き継ぐ）
+. ./essential.sh
+. ./development.sh
+. ./utility_apps.sh
 
 # 不要になったパッケージの削除
 sudo apt autoremove -y
