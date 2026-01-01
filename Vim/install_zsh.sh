56a4e5e9b1edc0f85f7537227e3e438f37a3243e
kskdev <kosumikedev@gmail.com>
2026-01-01 21:17:09 +0900

    remove windows support.

--- Vim/install_zsh.sh
+++ Vim/install_zsh.sh
@@ -1,42 +1,21 @@
 #!/bin/sh
+#
+# macOS/Linux用 vimrc インストールスクリプト
+#
+# 前提条件:
+#   - macOS: brew install fzf ripgrep deno
+#   - Linux: apt install fzf ripgrep deno (または各パッケージマネージャで)
+#
 
-case ${OSTYPE} in
-  darwin*)
-    echo "Install vim plugins for MacOS. Let's Launch vim."
+set -e
 
-    SOURCE_VIMRC="./vimrc_alone"
-    TARGET_VIMRC="${HOME}/.vimrc"
-    TARGET_DEIN="${HOME}/.vim/dein/"
+SOURCE_VIMRC="./vimrc_alone"
+TARGET_VIMRC="${HOME}/.vimrc"
 
-    cp ${SOURCE_VIMRC} ${TARGET_VIMRC}
+# vimrc をホームディレクトリにコピー
+cp ${SOURCE_VIMRC} ${TARGET_VIMRC}
+echo "[DONE] Copied vimrc_alone to ${TARGET_VIMRC}"
 
-    SOURCE_FZF="./bins/fzf_bin/fzf-0.23.0-linux_amd64/fzf"
-    SOURCE_RIP="./bins/rg/ripgrep-12.1.1-x86_64-unknown-linux-musl/rg"
-    TARGET_FZF="${TARGET_DEIN}/repos/github.com/junegunn/fzf/bin/"
-
-    mkdir -p ${TARGET_FZF}
-    cp ${SOURCE_FZF} ${TARGET_FZF}
-    cp ${SOURCE_RIP} ${TARGET_FZF}
-    ;;
-
-  linux*)
-    echo "Install vim plugins for Linux. Let's Launch vim."
-
-    SOURCE_VIMRC="./vimrc_alone"
-    TARGET_VIMRC="${HOME}/.vimrc"
-    TARGET_DEIN="${HOME}/.vim/dein/"
-
-    cp ${SOURCE_VIMRC} ${TARGET_VIMRC}
-
-    vim -e -c ":silent! call dein#install() | :q"
-
-    SOURCE_FZF="./bins/fzf_bin/fzf-0.23.0-linux_amd64/fzf"
-    SOURCE_RIP="./bins/rg/ripgrep-12.1.1-x86_64-unknown-linux-musl/rg"
-    TARGET_FZF="${TARGET_DEIN}repos/github.com/junegunn/fzf/bin/"
-
-    mkdir -p ${TARGET_FZF}
-    cp ${SOURCE_FZF} ${TARGET_FZF}
-    cp ${SOURCE_RIP} ${TARGET_FZF}
-    ;;
-
-esac
+# プラグインの自動インストール (初回起動時)
+echo "[INFO] プラグインのインストールはvim起動時に自動で行われます"
+echo "[INFO] vim を起動してください"
